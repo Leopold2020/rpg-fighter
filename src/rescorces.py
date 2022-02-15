e6b85913b66fd6104c6fffb0f9c6de655e547220
Gossepojke <svedlund.oskar@gmail.com>
2022-02-15 15:12:40 +0100

    first lesson

--- src/rescorces.py
+++ src/rescorces.py
@@ -6,4 +6,41 @@ class Character:
         self.armor = armor
 
     def __str__(self) -> str:
-        return f"name: {self.name}\nhealth: {self.health}\nattack: {self.attack}\narmor: {self.armor} "
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor} "
+
+    def damage(self):
+        return self.attack
+
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+
+
+class Goblin:
+    def __init__(self, health, attack, armor):
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+
+    def __str__(self):
+        return f"Goblin: \nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor} "
+
+
+    def damage(self):
+        return self.attack
+
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
